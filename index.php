ac93fa842cd47b44a3826f3f891eb34906e84dc5
Caio Loureiro <caio.loureiro@hotmail.com>
2020-09-16 09:54:34 -0300

    Add files via upload
    Commit inicial

---
+++ index.php
@@ -0,0 +1,147 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Página em Branco Simples</title>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="css/estilo.css"/>
+	</head>
+	<body>
+		<div class="menu"></div>
+		<div class="div01">
+			<div class="div01-txt"></div>
+			<div class="div01-img"></div>
+			<div class="div01-triangulo"></div>
+		</div>
+		<div class="fundo02"></div>
+		<div class="fundo03"></div>
+		<div class="fundo04"></div>
+		<div class="fundo05"></div>
+		<div class="fundo06"></div>
+		<div class="fundo07"></div>
+		<div class="fundo08"></div>
+		<div class="fundo09"></div>
+		<div class="container">
+			<div class="div02">
+				<div class="div02-card"></div>
+				<div class="div02-video"></div>
+			</div>
+			<div class="div03">
+				<div class="div03-titulo01"></div>
+				<div class="div03-menina"></div>
+			</div>
+			<div class="div04">
+				<div class="div04-col">
+					<div class="div04-foto"></div>
+					<div class="div04-txt"></div>
+					<div class="div04-btn"></div>
+				</div>
+				<div class="div04-col">
+					<div class="div04-painel"></div>
+				</div>
+			</div>
+			<div class="div05">
+				<div class="div05-col">
+					<div class="div05-painel"></div>
+				</div>
+				<div class="div05-col">
+					<div class="div05-txt"></div>
+					<div class="div05-foto"></div>
+					<div class="div05-btn"></div>
+				</div>
+			</div>
+			<div class="div06">
+				<div class="div06-col">
+					<div class="div06-txt"></div>
+					<div class="div06-foto"></div>
+					<div class="div06-btn"></div>
+				</div>
+				<div class="div06-col">
+					<div class="div06-painel"></div>
+				</div>
+			</div>
+			<div class="div07">
+				<div class="div07-titulo"></div>
+				<div class="div07-img"></div>
+				<div class="div07-triangulo"></div>
+			</div>
+			<div class="div08">
+				<div class="div08-card"></div>
+				<div class="div08-card"></div>
+				<div class="div08-card"></div>
+			</div>
+			<div class="div09">
+				<div class="div09-titulo"></div>
+				<div class="div09-menu-campo">
+					<div class="div09-menu">
+						<div class="div09-menu-btn btn01"></div>
+						<div class="div09-menu-btn btn02"></div>
+						<div class="div09-menu-btn btn03"></div>
+					</div>
+					<div class="div09-menu-pagina01">
+						<div class="div09-menu-col">
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+						</div>
+						<div class="div09-menu-col">
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+						</div>
+					</div>
+					<div class="div09-menu-pagina02">
+						<div class="div09-menu-col">
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+						</div>
+						<div class="div09-menu-col">
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+						</div>
+					</div>
+					<div class="div09-menu-pagina03">
+						<div class="div09-menu-col">
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+						</div>
+						<div class="div09-menu-col">
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+							<div class="div09-menu-pagina-btn"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="div10">
+				<div class="div10-btn"></div>
+				<div class="div10-btn"></div>
+				<div class="div10-btn"></div>
+			</div>
+			<div class="footer"></div>
+		</div>
+		<div class="whatsapp"></div>
+		<script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+		<script type="text/javascript" src="js/motor.js"></script>	
+	</body>
+</html>
