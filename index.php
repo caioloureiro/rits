c07e421362f5d81b475cc788bcf313cbfe565cc3
Caio Loureiro <caio.loureiro@hotmail.com>
2020-09-16 17:48:19 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -8,40 +8,84 @@
 	</head>
 	<body>
 		<div class="menu">
-			<div class="menu-logo"></div>
-			<div class="menu-btn">Atendimento</div>
-			<div class="menu-btn">Proin</div>
+			<div class="menu-container">
+				<div class="menu-logo"></div>
+				<div class="menu-btn">Atendimento</div>
+				<div class="menu-btn">Proin</div>
+			</div>
 		</div>
 		<div class="div01">
-			<div class="div01-txt"></div>
+			<div class="div01-txt-campo">
+				<div class="div01-titulo">Consectetur adipiscing elit</div>
+				<div class="div01-txt"><b>Quisque tincidunt:</b> Quisque ultricies, tortor vitae tincidunt molestie, arcu enim imperdiet urna, id aliquet ipsum massa luctus ipsum.</div>
+				<div class="div01-btn btn">
+					<div class="btn-base"></div>
+					<div class="btn-up">QUERO ME candidatar</div>
+				</div>
+			</div>
 			<div class="div01-img"></div>
 			<div class="div01-triangulo"></div>
 		</div>
 		<div class="fundo02"></div>
 		<div class="fundo03"></div>
-		<div class="fundo04"></div>
-		<div class="fundo05"></div>
-		<div class="fundo06"></div>
-		<div class="fundo07"></div>
+		<div class="fundo04">
+			<div class="col fundo04-col"></div>
+			<div class="col"></div>
+		</div>
+		<div class="fundo05">
+			<div class="col"></div>
+			<div class="col fundo05-col"></div>
+		</div>
+		<div class="fundo06">
+			<div class="col fundo06-col"></div>
+			<div class="col"></div>
+		</div>
+		<div class="fundo07">
+			<div class="col"></div>
+			<div class="col fundo07-col"></div>
+		</div>
 		<div class="fundo08"></div>
 		<div class="fundo09"></div>
 		<div class="container">
 			<div class="div02">
-				<div class="div02-card"></div>
-				<div class="div02-video"></div>
+				<div class="div02-card">
+					<div class="div02-card-azul"></div>
+					<div class="div02-card-titulo">Proin eu massa odio?</div>
+					<div class="div02-card-texto"><b>Maecenas faucibus elementum ipsum nec rhoncus. Integer a tortor vitae felis lobortis rutrum.</b> Donec convallis turpis et tincidunt pretium. Integer placerat luctus euismod. Nunc congue magna vel elementum.</div>
+				</div>
+				<div class="div02-video">
+					<div class="div02-video-lente"></div>
+					<div class="div02-video-play"></div>
+				</div>
 			</div>
 			<div class="div03">
-				<div class="div03-titulo01"></div>
+				<div class="div03-titulo01">Ut eu dolor vitae elit bibendum fringilla elit...</div>
 				<div class="div03-menina"></div>
 			</div>
 			<div class="div04">
 				<div class="div04-col">
 					<div class="div04-foto"></div>
-					<div class="div04-txt"></div>
-					<div class="div04-btn"></div>
+					<div class="div04-txt"><span>Suspendisse feugiat orci sed velit pulvinar.</span> Aliquam vitae lorem?</div>
+					<div class="div04-btn btn">
+						<div class="btn-base"></div>
+						<div class="btn-up">QUERO ME candidatar</div>
+					</div>
 				</div>
 				<div class="div04-col">
-					<div class="div04-painel"></div>
+					<div class="div04-painel">
+						<div class="div04-painel-ln">
+							<div class="div04-painel-icone" style="background-image:url('img/icone01.svg')"></div>
+							<div class="div04-painel-txt"><b>Maecenas faucibus elementum ipsum nec rhoncus,</b> Nullam sit amet pretium justo, metus.</div>
+						</div>
+						<div class="div04-painel-ln">
+							<div class="div04-painel-icone" style="background-image:url('img/icone02.svg')"></div>
+							<div class="div04-painel-txt"><b>Nunc congue</b> magna vel elementum'</div>
+						</div>
+						<div class="div04-painel-ln">
+							<div class="div04-painel-icone" style="background-image:url('img/icone03.svg')"></div>
+							<div class="div04-painel-txt"><b>Donec convallis</b> turpis et tincidunt pretium.</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="div05">
